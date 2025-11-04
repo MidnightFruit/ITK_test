10e416adceea6cb063d826bac4ab2306456d6649
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2025-11-05 06:40:01 +0700

    Написан тест для создания кошелька

--- terminal/tests.py
+++ terminal/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from terminal.models import *
+
+
+class WalletAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.wallet = Wallet.objects.create(amount=100)
+        self.wallet_uuid = self.wallet.id
+
+    def test_create_wallet(self):
+        """
+        Тестирование создания нового кошелька
+        """
+        data = {
+            "amount": 1000,
+        }
+        response = self.client.post('/api/v1/wallets/create/', data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
